5db200af13d2d579eeb27a47a31592806d590d36
--global <oladelemiro07@gmail.com>
2023-04-23 01:38:00 -0500

    adds platforms, borders, and dirt

---
+++ Assets/Scripts/PlatformMover.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+
+public class PlatformMover : MonoBehaviour
+{
+    /// <summary>The objects initial position.</summary>
+    private Vector2 startPosition;
+    /// <summary>The objects updated position for the next frame.</summary>
+    private Vector2 newPosition;
+
+    /// <summary>The speed at which the object moves.</summary>
+    [SerializeField] private float speed = 1.8f;
+    /// <summary>The maximum distance the object may move in either y direction.</summary>
+    [SerializeField] private float maxDistance = 2f;
+
+
+    void Start()
+    {
+        startPosition = transform.position;
+        newPosition = transform.position;
+    }
+
+    void Update()
+    {
+        newPosition.x = startPosition.x + (maxDistance * Mathf.Sin(Time.time * speed));
+        transform.position = newPosition;
+    }
+}
+
